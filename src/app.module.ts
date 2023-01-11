0048e415344657597387343b4ca6da49380225e3
IvanBabakov <46903548+IvanBabakov@users.noreply.github.com>
2023-01-11 15:43:04 +0300

    my-library

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BooksMongoDbModule } from './books-mongo-db/books-mongo-db.module';
+import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGO_COLLECTION),
-    BooksMongoDbModule,
-  ],
+  imports: [BooksModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { BooksController } from './books.controller';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BooksMongoDbModule } from 'src/books-mongo-db/books-mongo-db.module';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_COLLECTION),
+    BooksMongoDbModule,
+  ],
   providers: [BooksService],
   controllers: [BooksController],
 })
